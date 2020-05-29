f03a8f86508c4515c9d9d86aa0e1a3f891f2530d
mokeaveny <michaelkeaveny0@gmail.com>
2020-05-29 12:03:46 +0100

    Added working win conditions and player objects that switch with every move so the symbols can be compared and updated correctly

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -18,12 +18,58 @@ const gameBoard = (() => {
 	const getBoard = function() {
 		return board
 	}
+
+	const checkWin = function () {
+		// Checks top row
+		compareBoardSymbols(0, 1, 2);
+		// Checks middle row
+		compareBoardSymbols(3, 4, 5);
+		// Checks boottom row
+		compareBoardSymbols(6, 7, 8);
+		// Checks first column
+		compareBoardSymbols(0, 3, 6);
+		// Checks second column
+		compareBoardSymbols(1, 4, 7);
+		// Checks third column
+		compareBoardSymbols(2, 5, 8);
+		// Checks diagonal, top left to bottom right
+		compareBoardSymbols(0, 4, 8);
+		// Checks diagonal, top right to bottom left
+		compareBoardSymbols(2, 4, 6);
+
+		checkTie();
+	}
+
+	const compareBoardSymbols = function(p1, p2, p3) {
+		currentPlayer = gameController.getCurrentPlayer()
+		if (board[p1] == currentPlayer.symbol && board[p2] == currentPlayer.symbol && board[p3] == currentPlayer.symbol)
+		{
+			console.log("WON");
+		}
+	}
+
+	const checkTie = function() {
+
+		tie = true;
+
+		// Loops through every element in the array to see if any of them are still empty. If one is still empty then it isn't a tie.
+		for(i = 0; i < 9; i++) {
+			if (board[i] == "") {
+				tie = false
+			}
+		}
+
+		if(tie == true) {
+			console.log("TIE");
+		}
+	}
 		
 	return {
 		newGame,
 		getCell,
 		setCell,
-		getBoard
+		getBoard,
+		checkWin
 	}
 	
 })();
@@ -80,30 +126,66 @@ const displayController = (() => {
 
 const gameController = (() => {
 	const newGame = function() {
+
+		p1 = Player("p1", "X");
+		p2 = Player("p2", "O");
+		currentPlayer = p1;
+		console.log(currentPlayer);
+
+		console.log(p1.symbol);
+		console.log(p2.symbol);
+
 		gameBoard.newGame()
 		displayController.newGame()
+	}
+
+	const switchPlayer = function() {
+		if (currentPlayer == p1) {
+			currentPlayer = p2;
+		}
+		else {
+			currentPlayer = p1;
+		}
+	}
+
+	const getCurrentPlayer = function() {
+		return currentPlayer
 	}
 
 	// Fill board takes the clicked target and uses it to create a paragraph element which will contain the players symbol
 	const fillBoard = (e) => {
 		currentSquare = e.target
-		textElement = document.createElement("p")
-		textElement.textContent = "X"
-		currentSquare.appendChild(textElement)
 		boardIndex = currentSquare.getAttribute("board-index")
-		gameBoard.setCell(boardIndex, textElement.textContent)
-		console.log(gameBoard.getCell(boardIndex));
+		currentSymbol = gameBoard.getCell(boardIndex)
+
+		if (currentSymbol == "") {
+			textElement = document.createElement("p")
+			textElement.textContent = currentPlayer.symbol;
+			currentSquare.appendChild(textElement)
+			gameBoard.setCell(boardIndex, textElement.textContent)
+			gameBoard.checkWin();
+			// Once the symbol has been placed then the player is swapped so that they can then make a move
+			switchPlayer()
+			console.log(gameBoard.getCell(boardIndex));
+		}
 	}
 
 	return {
 		newGame,
-		fillBoard
+		fillBoard,
+		getCurrentPlayer
 	}
 
 })();
 
-const Player = (name, symbol) => {
-
+const Player = function(theName, theSymbol) {
+	name = theName;
+	symbol = theSymbol;
+	
+	return {
+		name,
+		symbol
+	}
 };
 
 gameController.newGame();
